3ccceee70ae1a1692f7638772e036bfb1ae0d68d
tequ1lAneio <evanlgp@hotmail.com>
2019-09-07 08:59:17 +0800

    document names modified

--- js/keywords.js
+++ js/keywords.js
@@ -1,2 +1,19 @@
 // instanceof
+const myInstanceof = function(a, b) {
+    while (true) {
+        if (b.__proto__ === b.prototype) {
+            return true
+        }
+        if (a.__proto___ === null) {
+            return false
+        }
+        b = b.__proto__
+    }
+}
 
+// new
+const myNew = (func, ... args) => {
+    let obj = {}
+    obj.__proto__ = func.prototype
+    obj.apply(obj, args)
+}
